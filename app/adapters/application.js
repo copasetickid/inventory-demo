63de9ce248d41bbacb84adcc1261213c88922bad
Rushaine McBean <rushaine.mcbean@gmail.com>
2016-07-19 18:55:20 -0400

    Fall back to  dasherize the model name

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -3,9 +3,6 @@ import Ember from 'ember';
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
 export default JSONAPIAdapter.extend(DataAdapterMixin,{
-  authorizer: 'authorizer:devise',
-  pathForType: function(type) {
-    return Ember.String.pluralize(Ember.String.underscore(type));
-  }
+  authorizer: 'authorizer:devise'
 });
 
